9dd3f15ea3b8df19b2056a665456a5d9d04b25a7
bryanemmanuel <bryan.emmanuel.paz@gmail.com>
2022-04-05 23:58:26 +0800

    first commit

---
+++ js/jquery.lightbox.js
@@ -0,0 +1,396 @@
+/*!
+ * jquery.lightbox.js
+ * https://github.com/duncanmcdougall/Responsive-Lightbox
+ * Copyright 2013 Duncan McDougall and other contributors;
+ *
+ * Options: 
+ * margin - int - default 50. Minimum margin around the image
+ * nav - bool - default true. enable navigation
+ * blur - bool - default true. Blur other content when open using css filter
+ * minSize - int - default 0. Min window width or height to open lightbox. Below threshold will open image in a new tab.
+ *
+ */
+(function ($) {
+
+    'use strict';
+
+    $.fn.lightbox = function (options) {
+
+        var opts = {
+            margin: 50,
+            nav: true,
+            blur: true,
+            minSize: 0
+        };
+
+        var plugin = {
+
+            items: [],
+            lightbox: null,
+            image: null,
+            current: null,
+            locked: false,
+            caption: null,
+			
+            init: function (items) {
+                plugin.items = items;
+				plugin.selector = "lightbox-"+Math.random().toString().replace('.','');
+
+                if (!plugin.lightbox) {
+                    $('body').append(
+                      '<div id="lightbox" style="display:none;">'+
+                      '<a href="#" class="lightbox-close lightbox-button"></a>' +
+                      '<div class="lightbox-nav">'+
+                      '<a href="#" class="lightbox-previous lightbox-button"></a>' +
+                      '<a href="#" class="lightbox-next lightbox-button"></a>' +
+                      '</div>' +
+                      '<div href="#" class="lightbox-caption"><p></p></div>' +
+                      '</div>'
+                    );
+
+                    plugin.lightbox = $("#lightbox");
+                    plugin.caption = $('.lightbox-caption', plugin.lightbox);
+                }
+
+                if (plugin.items.length > 1 && opts.nav) {
+                    $('.lightbox-nav', plugin.lightbox).show();
+                } else {
+                    $('.lightbox-nav', plugin.lightbox).hide();
+                }
+
+                plugin.bindEvents();
+
+            },
+
+            loadImage: function () {
+                if(opts.blur) {
+                    $("body").addClass("blurred");
+                }
+                $("img", plugin.lightbox).remove();
+                plugin.lightbox.fadeIn('fast').append('<span class="lightbox-loading"></span>');
+
+                var img = $('<img src="' + $(plugin.current).attr('href') + '" draggable="false">');
+
+                $(img).load(function () {
+                    $('.lightbox-loading').remove();
+                    plugin.lightbox.append(img);
+                    plugin.image = $("img", plugin.lightbox).hide();
+                    plugin.resizeImage();
+                    plugin.setCaption();
+                });
+            },
+
+            setCaption: function () {
+                var caption = $(plugin.current).data('caption');
+                if(!!caption && caption.length > 0) {
+                    plugin.caption.fadeIn();
+                    $('p', plugin.caption).text(caption);
+                }else{
+                    plugin.caption.hide();
+                }
+            },
+
+            resizeImage: function () {
+                var ratio, wHeight, wWidth, iHeight, iWidth;
+                wHeight = $(window).height() - opts.margin;
+                wWidth = $(window).outerWidth(true) - opts.margin;
+                plugin.image.width('').height('');
+                iHeight = plugin.image.height();
+                iWidth = plugin.image.width();
+                if (iWidth > wWidth) {
+                    ratio = wWidth / iWidth;
+                    iWidth = wWidth;
+                    iHeight = Math.round(iHeight * ratio);
+                }
+                if (iHeight > wHeight) {
+                    ratio = wHeight / iHeight;
+                    iHeight = wHeight;
+                    iWidth = Math.round(iWidth * ratio);
+                }
+
+                plugin.image.width(iWidth).height(iHeight).css({
+						'top': ($(window).height() - plugin.image.outerHeight()) / 2 + 'px',
+						'left': ($(window).width() - plugin.image.outerWidth()) / 2 + 'px'
+					}).show();
+                plugin.locked = false;
+            },
+
+            getCurrentIndex: function () {
+                return $.inArray(plugin.current, plugin.items);
+            },
+
+            next: function () {
+                if (plugin.locked) {
+                    return false;
+                }
+                plugin.locked = true;
+                if (plugin.getCurrentIndex() >= plugin.items.length - 1) {
+                    $(plugin.items[0]).click();
+                } else {
+                    $(plugin.items[plugin.getCurrentIndex() + 1]).click();
+                }
+            },
+
+            previous: function () {
+                if (plugin.locked) {
+                    return false;
+                }
+                plugin.locked = true;
+                if (plugin.getCurrentIndex() <= 0) {
+                    $(plugin.items[plugin.items.length - 1]).click();
+                } else {
+                    $(plugin.items[plugin.getCurrentIndex() - 1]).click();
+                }
+            },
+
+            bindEvents: function () {
+                $(plugin.items).click(function (e) {
+                    if(!$("#lightbox").is(":visible") && ($(window).width() < opts.minSize || $(window).height() < opts.minSize)) {
+                        $(this).attr("target", "_blank");
+                        return;
+                    }
+                    var self = $(this)[0];
+                    e.preventDefault();
+                    plugin.current = self;
+                    plugin.loadImage();
+
+                    // Bind Keyboard Shortcuts
+                    $(document).on('keydown', function (e) {
+                        // Close lightbox with ESC
+                        if (e.keyCode === 27) {
+                            plugin.close();
+                        }
+                        // Go to next image pressing the right key
+                        if (e.keyCode === 39) {
+                            plugin.next();
+                        }
+                        // Go to previous image pressing the left key
+                        if (e.keyCode === 37) {
+                            plugin.previous();
+                        }
+                    });
+                });
+
+                // Add click state on overlay background only
+                plugin.lightbox.on('click', function (e) {
+                    if (this === e.target) {
+                        plugin.close();
+                    }
+                });
+
+                // Previous click
+                $(plugin.lightbox).on('click', '.lightbox-previous', function () {
+                    plugin.previous();
+                    return false;
+                });
+
+                // Next click
+                $(plugin.lightbox).on('click', '.lightbox-next', function () {
+                    plugin.next();
+                    return false;
+                });
+
+                // Close click
+                $(plugin.lightbox).on('click', '.lightbox-close', function () {
+                    plugin.close();
+                    return false;
+                });
+
+                $(window).resize(function () {
+                    if (!plugin.image) {
+                        return;
+                    }
+                    plugin.resizeImage();
+                });
+            },
+
+            close: function () {
+                $(document).off('keydown'); // Unbind all key events each time the lightbox is closed
+                $(plugin.lightbox).fadeOut('fast');
+                $('body').removeClass('blurred');
+            }
+        };
+
+        $.extend(opts, options);
+
+        plugin.init(this);
+    };
+
+})(jQuery);
+
+
+
+if (typeof Object.create !== 'function') {
+    Object.create = function(obj) {
+        function F() {}
+        F.prototype = obj;
+        return new F();
+    };
+}
+
+(function($, window, document, undefined) {
+    "use strict";
+    
+    var SinglePageNav = {
+        
+        init: function(options, container) {
+            
+            this.options = $.extend({}, $.fn.singlePageNav.defaults, options);
+            
+            this.container = container;            
+            this.$container = $(container);
+            this.$links = this.$container.find('a');
+
+            if (this.options.filter !== '') {
+                this.$links = this.$links.filter(this.options.filter);
+            }
+
+            this.$window = $(window);
+            this.$htmlbody = $('html, body');
+            
+            this.$links.on('click.singlePageNav', $.proxy(this.handleClick, this));
+
+            this.didScroll = false;
+            this.checkPosition();
+            this.setTimer();
+        },
+
+        handleClick: function(e) {
+            var self  = this,
+                link  = e.currentTarget,
+                $elem = $(link.hash);  
+
+            e.preventDefault();             
+
+            if ($elem.length) { // Make sure the target elem exists
+
+                
+                // Prevent active link from cycling during the scroll
+                self.clearTimer();
+
+                // Before scrolling starts
+                if (typeof self.options.beforeStart === 'function') {
+                    self.options.beforeStart();
+                }
+
+                self.setActiveLink(link.hash);
+                
+                self.scrollTo($elem, function() { 
+                 
+                    if (self.options.updateHash) {
+                        document.location.hash = link.hash;
+                    }
+
+                    self.setTimer();
+
+                    // After scrolling ends
+                    if (typeof self.options.onComplete === 'function') {
+                        self.options.onComplete();
+                    }
+                });                            
+            }     
+        },
+        
+        scrollTo: function($elem, callback) {
+            var self = this;
+            var target = self.getCoords($elem).top;
+            var called = false;
+
+            self.$htmlbody.stop().animate(
+                {scrollTop: target}, 
+                { 
+                    duration: self.options.speed,
+                    easing: self.options.easing, 
+                    complete: function() {
+                        if (typeof callback === 'function' && !called) {
+                            callback();
+                        }
+                        called = true;
+                    }
+                }
+            );
+        },
+        
+        setTimer: function() {
+            var self = this;
+            
+            self.$window.on('scroll.singlePageNav', function() {
+                self.didScroll = true;
+            });
+            
+            self.timer = setInterval(function() {
+                if (self.didScroll) {
+                    self.didScroll = false;
+                    self.checkPosition();
+                }
+            }, 250);
+        },        
+        
+        clearTimer: function() {
+            clearInterval(this.timer);
+            this.$window.off('scroll.singlePageNav');
+            this.didScroll = false;
+        },
+        
+        // Check the scroll position and set the active section
+        checkPosition: function() {
+            var scrollPos = this.$window.scrollTop();
+            var currentSection = this.getCurrentSection(scrollPos);
+            this.setActiveLink(currentSection);
+        },        
+        
+        getCoords: function($elem) {
+            return {
+                top: Math.round($elem.offset().top) - this.options.offset
+            };
+        },
+        
+        setActiveLink: function(href) {
+            var $activeLink = this.$container.find("a[href='" + href + "']");
+                            
+            if (!$activeLink.hasClass(this.options.currentClass)) {
+                this.$links.removeClass(this.options.currentClass);
+                $activeLink.addClass(this.options.currentClass);
+            }
+        },        
+        
+        getCurrentSection: function(scrollPos) {
+            var i, hash, coords, section;
+            
+            for (i = 0; i < this.$links.length; i++) {
+                hash = this.$links[i].hash;
+                
+                if ($(hash).length) {
+                    coords = this.getCoords($(hash));
+                    
+                    if (scrollPos >= coords.top - this.options.threshold) {
+                        section = hash;
+                    }
+                }
+            }
+            
+            // The current section or the first link
+            return section || this.$links[0].hash;
+        }
+    };
+    
+    $.fn.singlePageNav = function(options) {
+        return this.each(function() {
+            var singlePageNav = Object.create(SinglePageNav);
+            singlePageNav.init(options, this);
+        });
+    };
+    
+    $.fn.singlePageNav.defaults = {
+        offset: 0,
+        threshold: 120,
+        speed: 400,
+        currentClass: 'current',
+        easing: 'swing',
+        updateHash: false,
+        filter: '',
+        onComplete: false,
+        beforeStart: false
+    };
+    
+})(jQuery, window, document);
